d15a8d3a2d7f71939b4b7f88fff0959095108853
WashingtonSouza <thewashington.souza@gmail.com>
2017-07-03 10:55:44 -0300

    Finalização projeto.

--- LojaRazor/App_Start/BundleConfig.cs
+++ LojaRazor/App_Start/BundleConfig.cs
@@ -8,7 +8,16 @@ namespace LojaRazor
         // For more information on Bundling, visit http://go.microsoft.com/fwlink/?LinkId=254725
         public static void RegisterBundles(BundleCollection bundles)
         {
-            
+            StyleBundle estilos = new StyleBundle("~/bundles/estilos");
+            estilos.Include("~/Content/bootstrap/css/bootstrap.css");
+            estilos.Include("~/Content/Style.css");
+            bundles.Add(estilos);
+
+            StyleBundle scripts = new StyleBundle("~/bundles/scripts");
+            scripts.Include("~/Scripts/jquery-1.7.1.js");
+            scripts.Include("~/Scripts/jquery.Validate.js");
+            scripts.Include("~/Scripts/jquery.unobtrusive.js");
+            bundles.Add(scripts);
         }
     }
 }
